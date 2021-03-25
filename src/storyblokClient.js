25dfb33b9c996ada4460171c67306b2256db4878
James Lovallo <james.lovallo@mrm.com>
2021-03-25 10:52:30 -0400

    initial commit

--- src/storyblokClient.js
+++ src/storyblokClient.js
@@ -1,11 +1,11 @@
 import StoryblokClient from 'storyblok-js-client'
 
 const client = new StoryblokClient({
-  accessToken: '<YOUR_TOKEN>' // replace with your accessToken
+	accessToken: '0rSqEsI9IEpvyMG9hrk3BAtt', // replace with your accessToken
 })
 
 export const defaultRequestConfig = {
-  version: 'draft'
+	version: 'draft',
 }
 
 export default client
