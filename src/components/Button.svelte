a007e04b4dde33a51dd6ccd321e8f5ab570bbdeb
James Lovallo <james.lovallo@mrm.com>
2021-04-02 11:50:31 -0400

    add ripple effect

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,20 +1,26 @@
 <script>
-	import { colors, padding, rounded, shadow, editable } from './utils'
+	import { colors, padding, rounded, shadow } from './utils'
 	import Link from './Link.svelte'
 	export let blok
+	$: hidden = blok.hide_text ? 'sr-only' : ''
+
+	function side(b) {
+		if (b.icon) {
+			return b.icon_on_right ? 'right' : 'left'
+		}
+	}
 
 	function styles(b) {
-		return colors(b) + padding(b, true) + rounded(b) + shadow(b) + b.style
+		let wide = b.hide_text ? false : true
+		return colors(b) + padding(b, wide) + rounded(b) + shadow(b) + b.style
 	}
 </script>
 
 <Link
-	classes="button colors {blok.class} {blok.icon && blok.text
-		? 'pad-icon'
-		: ''}"
+	classes="button colors {side(blok)} {blok.class}"
 	styles={styles(blok)}
 	{blok}
 >
-	{@html blok.icon}
-	{blok.text}
+	<span aria-hidden="true">{@html blok.icon}</span>
+	<span class="button-text {hidden}">{blok.text}</span>
 </Link>
--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,21 +1,68 @@
 <script>
 	import { link, editable } from './utils'
 	export let blok, classes, styles
+
+	function createRipple(event) {
+		const el = event.currentTarget
+		const circle = document.createElement('span')
+		const diameter = Math.max(el.clientWidth, el.clientHeight)
+		const radius = diameter / 2
+
+		circle.style.width = circle.style.height = `${diameter}px`
+		circle.style.left = `${event.clientX - el.offsetLeft - radius}px`
+		circle.style.top = `${event.clientY - el.offsetTop - radius}px`
+		circle.classList.add('ripple')
+
+		const ripple = el.querySelector('.ripple')
+
+		if (ripple) {
+			ripple.remove()
+		}
+
+		el.appendChild(circle)
+	}
 </script>
 
 {#if link(blok.action).href}
 	<a
-		class={classes}
+		class="{classes} Link"
 		style={styles}
 		href={link(blok.action).href}
 		aria-label={link(blok.action).aria}
 		rel={link(blok.action).prefetch ? 'prefetch' : ''}
+		on:click={blok.ripple ? createRipple : undefined}
 		use:editable={blok}
 	>
 		<slot />
 	</a>
 {:else}
-	<div class={classes} style={styles} use:editable={blok}>
+	<div
+		class="{classes} Link"
+		style={styles}
+		on:click={blok.ripple ? createRipple : undefined}
+		use:editable={blok}
+	>
 		<slot />
 	</div>
 {/if}
+
+<style>
+	.Link {
+		overflow: hidden;
+	}
+	:global(span.ripple) {
+		position: absolute;
+		border-radius: 50%;
+		transform: scale(0);
+		animation: ripple 600ms linear;
+		background-color: currentColor;
+		opacity: 0.75;
+	}
+
+	@keyframes ripple {
+		to {
+			transform: scale(4);
+			opacity: 0;
+		}
+	}
+</style>
