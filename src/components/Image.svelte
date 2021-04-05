6945cd5e158d2a327bc85b08e369095e58ad0626
James Lovallo <james.lovallo@mrm.com>
2021-04-04 22:17:39 -0400

    test srcsets

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,11 +1,11 @@
 <script>
-	import { onMount } from 'svelte'
+	// import { onMount } from 'svelte'
 	import { editable } from './utils'
-	let debounce = require('lodash/debounce')
+	// let debounce = require('lodash/debounce')
 
 	export let blok
 	let el
-	let width = 0
+	// let width = 0
 
 	function style(b) {
 		let filters = `filter: brightness(${b.brightness}) contrast(${b.contrast}) saturate(${b.saturation});`
@@ -28,26 +28,36 @@
 			: ''
 		let smart = b.fit === 'smart' && !b.image.focus ? 'smart/' : ''
 		let grayscale = b.grayscale ? ':grayscale()' : ''
-		if (w && b.image.filename && b.image.filename.endsWith('.svg')) {
+		if (b.image.filename && b.image.filename.endsWith('.svg')) {
 			return b.image.filename
-		} else if (w && b.image.filename) {
+		} else if (b.image.filename) {
 			return `https://img2.storyblok.com/${fit}${w}x${height}/${smart}filters:format(webp):fill(transparent):quality(${b.quality}):focal(${b.image.focus})${grayscale}${image}`
-		} else if (w) {
+		} else {
 			return `https://picsum.photos/seed/{${b._uid}}picsum/${w}/${height}.webp`
 		}
 	}
 
-	function setWidth() {
-		let perfectWidth =
-			Math.round((el.clientWidth * devicePixelRatio) / 100) * 100
-		if (perfectWidth !== 0 && perfectWidth > width) {
-			width = perfectWidth
-		} else {
-			width = blok.width || 600
+	function srcset(b) {
+		let srcset = []
+		if (!b.width || b.image.filename.endsWith('.svg')) {
+			for (let i = 1; i <= 20; i++) {
+				srcset.push(src(b, i * 100) + (' ' + i * 100 + 'w'))
+			}
 		}
+		return srcset.join(', ')
 	}
 
-	onMount(setWidth)
+	// function setWidth() {
+	// 	let perfectWidth =
+	// 		Math.round((el.clientWidth * devicePixelRatio) / 100) * 100
+	// 	if (perfectWidth !== 0 && perfectWidth > width) {
+	// 		width = perfectWidth
+	// 	} else {
+	// 		width = blok.width || 600
+	// 	}
+	// }
+
+	// onMount(setWidth)
 </script>
 
 <div
@@ -55,19 +65,19 @@
 	style="{style(blok)} {blok.width > 0 ? `width: ${blok.width}px` : ''}"
 	bind:this={el}
 >
-	{#if width > 0}
-		<img
-			src={src(blok, width)}
-			alt=""
-			loading="lazy"
-			width={blok.width || '100%'}
-			use:editable={blok}
-		/>
-	{/if}
+	<!-- {#if width > 0} -->
+	<img
+		src={src(blok, 600)}
+		srcset={srcset(blok)}
+		alt=""
+		loading="lazy"
+		width={blok.width || '100%'}
+		use:editable={blok}
+	/>
+	<!-- {/if} -->
 </div>
 
-<svelte:window on:resize={debounce(setWidth, 500)} />
-
+<!-- <svelte:window on:resize={debounce(setWidth, 500)} /> -->
 <style>
 	.image {
 		position: relative;
