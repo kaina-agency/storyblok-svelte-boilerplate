f027d6455f6ddef7c06f667ce50d6eae0464a770
James Lovallo <patrickonparker@gmail.com>
2021-05-13 14:11:07 -0400

    form fix

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -26,10 +26,7 @@
 	<input type="hidden" name="_next" value={next} />
 	<input type="hidden" name="_template" value="box" />
 	<input type="text" name="_honey" style="display: none" />
-	<a
-		style="display: none;"
-		href={`${window.location.origin}/${blok.success_page.cached_url}`}
-	>
+	<a style="display: none;" href={blok.success_page.cached_url}>
 		Success page, since sapper has to crawl every damn page.
 	</a>
 	{#each blok.content as blok}
